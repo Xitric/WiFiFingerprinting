c2b404dc90ab4371ae7ac9ecd07e46c0f5faa694
Emil Nielsen <emilqz@gmail.com>
2020-04-23 08:26:23 +0200

    NnManager

---
+++ app/src/main/java/dk/sdu/fingerprinting/nearest_neighbor/NnManager.java
@@ -0,0 +1,48 @@
+package dk.sdu.fingerprinting.nearest_neighbor;
+
+import android.util.Pair;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dk.sdu.fingerprinting.FingerprintingDatabase;
+
+public class NnManager {
+
+    private FingerprintingDatabase database;
+
+    public NnManager(FingerprintingDatabase database) {
+        this.database = database;
+    }
+
+    private LiveData<String> getLocation() {
+        // Location = classifier
+
+        return Transformations.map(database.trainingDataDao().getAll(), this::knn);
+    }
+
+    private String knn(List<TrainingData> trainingData) {
+        // Mac address with a list of signals strengths
+        Map<String, List<Integer>> macSignalStrengths = new HashMap<>();
+        for (TrainingData data : trainingData) {
+            for (Pair macStrength: data.signalStrengths) {
+                String macAddress = (String) macStrength.first;
+                int signalStrength = (int) macStrength.second;
+                if (!macSignalStrengths.containsKey(macAddress)){
+                    macSignalStrengths.put(macAddress, Arrays.asList(signalStrength));
+                } else {
+                    macSignalStrengths.get(macAddress).add(signalStrength);
+                }
+            }
+        }
+        return "location";
+    }
+
+
+}
